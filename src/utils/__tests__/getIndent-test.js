afa11e4fd1b7d91ddfafce888cfc97ab8f18a4e8
Frankie Bagnardi <f.bagnardi@gmail.com>
2017-04-20 03:00:12 -0700

    lint and npm scripts

--- src/utils/__tests__/getIndent-test.js
+++ src/utils/__tests__/getIndent-test.js
@@ -7,18 +7,18 @@ ${indent}${indent}bar
 ${indent}baz
 `;
 
-it('works for one space', () => {
-  expect(getIndent(makeStr(' '))).toBe(' ');
+it(`works for one space`, () => {
+  expect(getIndent(makeStr(` `))).toBe(` `);
 });
 
-it('works for two spaces', () => {
-  expect(getIndent(makeStr('  '))).toBe('  ');
+it(`works for two spaces`, () => {
+  expect(getIndent(makeStr(`  `))).toBe(`  `);
 });
 
-it('works for four spaces', () => {
-  expect(getIndent(makeStr('    '))).toBe('    ');
+it(`works for four spaces`, () => {
+  expect(getIndent(makeStr(`    `))).toBe(`    `);
 });
 
-it('works for one tab', () => {
-  expect(getIndent(makeStr('\t'))).toBe('\t');
+it(`works for one tab`, () => {
+  expect(getIndent(makeStr(`\t`))).toBe(`\t`);
 });
